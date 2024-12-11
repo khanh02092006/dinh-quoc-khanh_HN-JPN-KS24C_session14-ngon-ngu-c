7e83f6f2c0e9603f3479c1e814a4690d8842491c
khanh02092006 <dinhkhanh1881@gmail.com>
2024-12-11 20:51:58 +0700

    session 14_ex2.

---
+++ session 14_ex2.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    char chuoi[] = "hello world"; 
+    for (int i = 0; chuoi[i] != '\0'; i++) { 
+        printf("%c ", chuoi[i]); 
+    }
+    printf("\n"); 
+    return 0;
+}
+
